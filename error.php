2ef73adec6d54b623e21843a126292489407c5c4
Xiong Gao <growdane@gmail.com>
2015-11-10 16:23:58 +0800

    modify logs

--- error.php
+++ error.php
@@ -1,5 +1,7 @@
 <?php
-require_once dirname(__FILE__) . '/common/GlobalFunctions.php'
+ini_set('display_errors', '1');
+
+require_once dirname(__FILE__) . '/common/GlobalFunctions.php';
 
 interface_log(INFO, EC_OK, "TEST OK");
 ?>
